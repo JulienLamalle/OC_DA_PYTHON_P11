22dc8c36e520c528e715f659c625d2a88f295b28
JulienLamalle <Julienlamalle63100@gmail.com>
2022-03-04 21:18:07 +0100

    delete unused condition

--- src/helper/helper.py
+++ src/helper/helper.py
@@ -2,8 +2,6 @@ import datetime
 
 
 def is_valid_purchase(competition, club, required_places):
-  if not competition or not club:
-    return False
   if required_places <= 0 or required_places > int(competition['numberOfPlaces']):
     return False
   if int(required_places) > int(club["points"]):
